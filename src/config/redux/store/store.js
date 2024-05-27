57ba17b10ddd5f7052491de47d4b13456cc212dc
hamzayaseenwd <hamzayaseen836@gmail.com>
2024-05-27 20:01:40 +0500

    Update in process

--- src/config/redux/store/store.js
+++ src/config/redux/store/store.js
@@ -1,6 +1,6 @@
-import { configureStore } from "@reduxjs/toolkit";
-import todoReducer from "../reducers/todoSlice";
+// import { configureStore } from "@reduxjs/toolkit";
+// import todoReducer from "../reducers/todoSlice";
 
-export const store = configureStore({
-    reducer: todoReducer
-});
+// export const store = configureStore({
+//     reducer: todoReducer
+// });
