2f88e3c18fb34979b85410f04934648bc7d4f1b1
hamzayaseenwd <hamzayaseen836@gmail.com>
2024-06-24 16:39:50 +0500

    Update

--- src/config/react_router_dom/RouterConfig.jsx
+++ src/config/react_router_dom/RouterConfig.jsx
@@ -5,17 +5,21 @@ import Login from '../../screens/Login'
 import Dashboard from '../../screens/Dashboard'
 import Signup from '../../screens/Signup'
 import Profile from '../../screens/Profile'
+import Navbar from '../../components/Navbar'
 
 const RouterConfig = () => {
   return (
     <BrowserRouter>
+        <Navbar/>
     
     <Routes>
+
         <Route path='' element={<Home/>}/>
         <Route path='login' element={<Login/>}/>
         <Route path='signup' element={<Signup/>}/>
         <Route path='dashboard' element={<Dashboard/>}/>
         <Route path='profile/:id' element={<Profile/>}/>
+        
     </Routes>
     
     </BrowserRouter>
--- src/screens/Profile.jsx
+++ src/screens/Profile.jsx
@@ -1,9 +1,63 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import Custom_Input_Field from '../components/Custom_Input_Field'
+import Custom_Button from '../components/Custom_Button'
 
 const Profile = () => {
-  return (
-    <div>Profile Dynamic</div>
+  const newPassword = useRef()
+  const confirmnewPassword = useRef()
+  
+//   function showUserProfile1() {
+//     const user = firebase.auth().currentUser;
+ 
+//     if (user !== null) {
+//         user.providerData.forEach((profile) => {
+//             console.log("Sign-in provider: " + profile.providerId);
+//             console.log("  Provider-specific UID: " + profile.uid);
+//             console.log("  Name: " + profile.displayName);
+//             console.log("  Email: " + profile.email);
+//             console.log("  Photo URL: " + profile.photoURL);
+//         });
+//     }
+// }
+ 
+// Method to update the password
+
+function UpdatePassword() {
+    const user = firebase.auth().currentUser;
+
+    if (newPassword === confirmnewPassword) {
+    
+    user.updatePassword(newPassword).then(() => {
+      console.log('Update SuccessFul');
+     }).catch((error) => {
+        console.log(error)
+    });
+  } else { 
+    console.log("Password not match")
+ }
+}
+  
+  
+ return (
+    <div>
+
+      <div>Profile Dynamic</div>
+      <div className='' >
+    <form className='ml-[25%] mb-[25%] justify-center items-center justify-items-center border-black'
+    onSubmit={UpdatePassword} >
+
+  <Custom_Input_Field
+  type="password" placeholder='Enter Your New Password' ref={newPassword}
+  />
+  <Custom_Input_Field
+  type="password" placeholder='Re-Enter Your NewPassword' ref={confirmnewPassword}
+  />
+  <Custom_Button
+  type="submit"
+  name= "Change Password"
+  />
+    </form>
+  </div></div>
   )
 }
-
 export default Profile
